b41479a9609a85437c49bfc5c16345870271eb33
Vizzyy <barnabas.vizy@gmail.com>
2020-12-01 01:21:26 -0500

    day1

--- day1/puzzle1.py
+++ day1/puzzle1.py
@@ -20,7 +20,7 @@ def puzzle1():
             if i == j:
                 continue
             if sanitized_inputs[i] + sanitized_inputs[j] == 2020:
-                print(count)
+                print(f"{count} comparisons")
                 return sanitized_inputs[i], sanitized_inputs[j]
             count += 1
     return "Not found"
@@ -37,7 +37,7 @@ def part2():
                 if y == j or y == i:
                     continue
                 if sanitized_inputs[i] + sanitized_inputs[j] + sanitized_inputs[y] == 2020:
-                    print(count)
+                    print(f"{count} comparisons")
                     return sanitized_inputs[i], sanitized_inputs[j], sanitized_inputs[y]
                 count += 1
     return 0, 0, 0
@@ -46,8 +46,10 @@ def part2():
 result = puzzle1()
 print(result)
 print(result[0] * result[1])
+# O N^2 , but given the sorted dataset only takes 407 comparisons
+
 
 result2 = part2()
 print(result2)
 print(result2[0] * result2[1] * result2[2])
-
+# O N^3 , but given the sorted dataset only takes 138 comparisons
