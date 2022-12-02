e88e503011a45cb08b105e85a3c80b36c3a3bc38
Vizzyy <barnabas.vizy@gmail.com>
2022-12-01 20:40:27 -0500

    golfed 2022-01 part two

--- 2022/01/puzzle2.py
+++ 2022/01/puzzle2.py
@@ -1,15 +1,1 @@
-with open('input.txt', 'r') as file:
-    inputs = file.read()
-
-inputs = inputs.strip().split('\n\n')
-
-inputs2 = [[int(num) for num in inp.split('\n')] for inp in inputs]
-
-sums = []
-for elf in inputs2:
-    temp = sum(elf)
-    sums.append(temp)
-
-sums.sort()
-
-print(sum(sums[-3:]))
+with open('i', 'r') as q: print(sum(sorted([sum(e) for e in [[int(n) for n in t.split('\n')] for t in q.read().strip().split('\n\n')]])[-3:]))
