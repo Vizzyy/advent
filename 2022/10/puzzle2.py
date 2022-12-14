5c208214c497d6afc631c1b5f48a5f7352b3997d
Vizzyy <barnabas.vizy@gmail.com>
2022-12-13 20:14:00 -0500

    Day 10 part 2

--- 2022/10/puzzle2.py
+++ 2022/10/puzzle2.py
@@ -1,4 +1,4 @@
-with open('test.txt', 'r') as file:
+with open('input.txt', 'r') as file:
     inputs = file.read()
 instructions = [instruction.split(' ') for instruction in inputs.strip().split('\n')]
 print(len(instructions))
@@ -6,7 +6,7 @@ print(len(instructions))
 cycle = 1
 X = 1
 cycle_values = {}
-screen = [['.' for x in range(40)] for y in range(6)]
+screen = [[' ' for x in range(40)] for y in range(6)]
 
 for idx in range(len(instructions)):
     instruction = instructions[idx]
@@ -27,18 +27,24 @@ print(cycle_values)
 print()
 
 for cycle in sorted(list(cycle_values.keys())):
+    value = cycle_values[cycle]
+    sprite_indexes = [value]
+    if value % 40 > 0:
+        sprite_indexes.append(value - 1)
+    if value % 40 != 0:
+        sprite_indexes.append(value + 1)
+    sprite_indexes.sort()
+
     x_pos = (cycle % 40) - 1 if cycle % 40 != 0 else 39
     y_pos = cycle // 40 if cycle % 40 != 0 else (cycle // 40) - 1
-    value = cycle_values[cycle]
-    # screen[y_pos][x_pos] = f'{value}'
-    # print(f'{cycle} : {[y_pos, x_pos]} = {value}')
-    try:
-        if x_pos > 0:
-            screen[y_pos][x_pos - 1] = '#' if value == cycle else '.'
-        screen[y_pos][x_pos] = '#' if value == cycle else '.'
-        if x_pos < 39:
-            screen[y_pos][x_pos + 1] = '#' if value == cycle else '.'
-    except:
-        pass
 
-[print(row) for row in screen]
+    crt_indexes = [x_pos]
+
+    print(f'{cycle} : {[y_pos, x_pos]} = {value}, crt_indexes: {crt_indexes}, sprite_indexes: {sprite_indexes}')
+    overlapping = list(set(sprite_indexes) & set(crt_indexes))
+    if len(overlapping) > 0:
+        print(f'overlapping: {overlapping}')
+    for index in overlapping:
+        screen[y_pos][index] = '#'
+
+[print(''.join(row)) for row in screen]
