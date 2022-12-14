6fd846288576b220e9daa50c5986b0c5dd50e1fe
Vizzyy <barnabas.vizy@gmail.com>
2022-12-13 23:00:10 -0500

    day 11 init

--- 2022/11/puzzle.py
+++ 2022/11/puzzle.py
@@ -1,4 +1,4 @@
 with open('test.txt', 'r') as file:
     inputs = file.read()
-inputs = inputs.strip().split('\n')
-print(inputs)
+inputs = [line.split('\n') for line in inputs.strip().split('\n\n')]
+[print(line) for line in inputs]
--- 2022/11/puzzle2.py
+++ 2022/11/puzzle2.py
@@ -1,4 +1,4 @@
-with open('input.txt', 'r') as file:
+with open('test.txt', 'r') as file:
     inputs = file.read()
-inputs = inputs.strip().split('\n')
-print(inputs)
+inputs = [line.split('\n') for line in inputs.strip().split('\n\n')]
+[print(line) for line in inputs]
