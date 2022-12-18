e8ac7cce678a4d48dd4e39e51f0afea92439e8e2
Vizzyy <barnabas.vizy@gmail.com>
2022-12-17 23:39:17 -0500

    naive solution

--- 2022/12/puzzle.py
+++ 2022/12/puzzle.py
@@ -1,14 +1,16 @@
 with open('test.txt', 'r') as file:
     grid = file.read()
 grid = [[*row] for row in grid.strip().split('\n')]
-[print(''.join(row)) for row in grid]
-print()
+# [print(''.join(row)) for row in grid]
+# print()
 grid = grid[::-1]
 
 start_pos = None
 end_pos = None
 IS_DEST_GREATER = False  # false == reverse
 RECURSION_LIMIT = 50
+shortest_path = None
+DEBUG = False
 
 memo = {}
 
@@ -24,7 +26,6 @@ for row in range(len(grid)):
         break
 
 print(f'start_pos: {start_pos}, end_pos: {end_pos}')
-print()
 
 # [print(row) for row in grid]
 
@@ -34,80 +35,124 @@ def get_possible_directions(current_pos, is_dest_value_greater):
     current_y = current_pos[0]
     start_value = grid[current_y][current_x]
     possible_directions = []
+    if DEBUG: print(f'\n[get_possible_directions] - current_pos: {current_pos}, start_value: {start_value}({ord(start_value)})')
 
     try:
         dest_y = current_y
         dest_x = current_x - 1
+        if dest_y >= len(grid) or dest_y < 0 or dest_x >= len(grid[0]) or dest_x < 0:
+            raise IndexError
         dest_value = grid[dest_y][dest_x]
-        print(
-            f'left: {[dest_y, dest_x]}({ord(dest_value)}), dest_value: {dest_value}, {ord(dest_value) <= ord(start_value)}')
-        if ord(dest_value) > ord(start_value) if is_dest_value_greater else ord(start_value) - ord(dest_value) <= 1:
+        if ord(dest_value) > ord(start_value) if is_dest_value_greater else abs(ord(start_value) - ord(dest_value)) <= 1:
+            if DEBUG: print(f'left: {[dest_y, dest_x]}({ord(dest_value)}), dest_value: {dest_value}, '
+                  f'{ord(dest_value) <= ord(start_value)}')
             possible_directions.append([dest_y, dest_x])
     except IndexError:
         pass
     try:
         dest_y = current_y
         dest_x = current_x + 1
+        if dest_y >= len(grid) or dest_y < 0 or dest_x >= len(grid[0]) or dest_x < 0:
+            raise IndexError
         dest_value = grid[dest_y][dest_x]
-        print(
-            f'right: {[dest_y, dest_x]}({ord(dest_value)}), dest_value: {dest_value}, {ord(dest_value) <= ord(start_value)}')
-        if ord(dest_value) > ord(start_value) if is_dest_value_greater else ord(start_value) - ord(dest_value) <= 1:
+        if ord(dest_value) > ord(start_value) if is_dest_value_greater else abs(ord(start_value) - ord(dest_value)) <= 1:
+            if DEBUG: print(f'right: {[dest_y, dest_x]}({ord(dest_value)}), dest_value: {dest_value}, '
+                  f'{ord(dest_value) <= ord(start_value)}')
             possible_directions.append([dest_y, dest_x])
     except IndexError:
         pass
     try:
         dest_y = current_y - 1
         dest_x = current_x
+        if dest_y >= len(grid) or dest_y < 0 or dest_x >= len(grid[0]) or dest_x < 0:
+            raise IndexError
         dest_value = grid[dest_y][dest_x]
-        print(
-            f'down: {[dest_y, dest_x]}({ord(dest_value)}), dest_value: {dest_value}, {ord(dest_value) <= ord(start_value)}')
-        if ord(dest_value) > ord(start_value) if is_dest_value_greater else ord(start_value) - ord(dest_value) <= 1:
+        if ord(dest_value) > ord(start_value) if is_dest_value_greater else abs(ord(start_value) - ord(dest_value)) <= 1:
+            if DEBUG: print(f'down: {[dest_y, dest_x]}({ord(dest_value)}), dest_value: {dest_value}, '
+                  f'{ord(dest_value) <= ord(start_value)}')
             possible_directions.append([dest_y, dest_x])
     except IndexError:
         pass
     try:
         dest_y = current_y + 1
         dest_x = current_x
+        if dest_y >= len(grid) or dest_y < 0 or dest_x >= len(grid[0]) or dest_x < 0:
+            raise IndexError
         dest_value = grid[dest_y][dest_x]
-        print(
-            f'up: {[dest_y, dest_x]}({ord(dest_value)}), dest_value: {dest_value}, {ord(dest_value) <= ord(start_value)}')
-        if ord(dest_value) > ord(start_value) if is_dest_value_greater else ord(start_value) - ord(dest_value) <= 1:
+        if ord(dest_value) > ord(start_value) if is_dest_value_greater else abs(ord(start_value) - ord(dest_value)) <= 1:
+            if DEBUG: print(f'up: {[dest_y, dest_x]}({ord(dest_value)}), dest_value: {dest_value}, '
+                  f'{ord(dest_value) <= ord(start_value)}')
             possible_directions.append([dest_y, dest_x])
     except IndexError:
         pass
 
-    print(f'[get_possible_directions] - current_pos: {current_pos}, start_value: {start_value}({ord(start_value)}), '
-          f'possible_directions: {possible_directions}')
-
     return possible_directions.copy()
 
 
-def traverse_backwards(current_pos, previous_pos, final_pos, steps_since_beginning):
+def traverse_backwards(current_pos, final_pos, path_traversed):
+    global memo
+    steps_since_beginning = len(path_traversed)
+    previous_pos = path_traversed[-1] if steps_since_beginning > 0 else None
+    internal_path = path_traversed.copy()
+    internal_path.append(current_pos)
+    if DEBUG: print(f'\n[traverse] - current: {current_pos}, prev: {previous_pos}, path: {internal_path}, steps: {steps_since_beginning}')
     possible_directions = get_possible_directions(current_pos, IS_DEST_GREATER)
-    print(f'[traverse_backwards] - current_pos: {current_pos}, possible_directions: {possible_directions}, '
-          f'steps_since_beginning: {steps_since_beginning}')
+    if previous_pos and previous_pos in possible_directions:
+        possible_directions.pop(possible_directions.index(previous_pos))
+    if DEBUG: print(f'possible: {possible_directions}')
 
     if steps_since_beginning > RECURSION_LIMIT:
-        print('We have reached the recursion limit!')
+        if DEBUG: print('We have reached the recursion limit!')
         return
 
     if current_pos == final_pos:
-        print('We have reached the terminus.')
-        return
+        if DEBUG: print('We have reached the terminus.')
 
-    if f'{current_pos[0]},{current_pos[1]}' in memo:
-        print('We have already mapped this location.')
+        # if it doesn't exist in memo, add it
+        if f'{current_pos[0]},{current_pos[1]}' not in memo.keys():
+            memo[f'{current_pos[0]},{current_pos[1]}'] = internal_path.copy()
+        # else if the current path is less than what is there, overwrite it
+        elif len(internal_path) < len(memo[f'{current_pos[0]},{current_pos[1]}']):
+            memo[f'{current_pos[0]},{current_pos[1]}'] = internal_path.copy()
+
         return
 
     for possible_direction in possible_directions:
-        if previous_pos and possible_direction == previous_pos:  # skip the direction we just came from
+        if possible_direction in path_traversed:
+            # print(f'Skipping {possible_direction} as it is already part of our path.')
             continue
-        memo[f'{current_pos[0]},{current_pos[1]}'] = steps_since_beginning + 1
-        print(f'possible_direction: {possible_direction}')
-        traverse_backwards(possible_direction, current_pos, final_pos, steps_since_beginning + 1)
+
+        # if it doesn't exist in memo, add it
+        if f'{current_pos[0]},{current_pos[1]}' not in memo.keys():
+            memo[f'{current_pos[0]},{current_pos[1]}'] = internal_path.copy()
+        # else if the current path is less than what is there, overwrite it
+        elif len(internal_path) < len(memo[f'{current_pos[0]},{current_pos[1]}']):
+            memo[f'{current_pos[0]},{current_pos[1]}'] = internal_path.copy()
+
+        if DEBUG: print(f'next direction: {possible_direction}')
+        traverse_backwards(possible_direction, final_pos, internal_path.copy())
 
 
-traverse_backwards(end_pos, None, start_pos, 0)
+traverse_backwards(end_pos, start_pos, [])
+
+# print()
+# [print(f'{key}: {memo[key]}, {len(memo[key])}') for key in memo]
+
+# step = 1
+# for key in memo.keys():
+#     row, column = key.split(',')
+#     print(row, column)
+#     grid[int(row)][int(column)] = f'{step:2}'
+#     step += 1
+#
+# [print(row) for row in grid]
+
+path_to_final = memo[f'{start_pos[0]},{start_pos[1]}'][1:]
+# for step in range(len(path_to_final)):
+#     grid[path_to_final[step][0]][path_to_final[step][1]] = f'{step:2}'
+#
+# print()
+# [print(row) for row in grid[::-1]]
 
 print()
-print(memo)
+print(f'{path_to_final}, {len(path_to_final)}')
