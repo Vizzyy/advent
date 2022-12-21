080182bbe66e4854edf924f11622e3916e8bdce3
Barney <barney@Barneys-Air.router>
2022-12-20 22:33:04 -0500

    progress

--- 2022/14/puzzle.py
+++ 2022/14/puzzle.py
@@ -8,4 +8,14 @@ with open('test.txt', 'r') as file:
 inputs = [[pair.split(',') for pair in line.split(' -> ')] for line in inputs.strip().split('\n')]
 [print(line) for line in inputs]
 
+
+grid = [''] * 1000
+grid = [grid.copy()] * 1000
+
+for instruction in inputs:
+    for pair in range(len(instruction)):
+        column = instruction[pair][0]
+        row = instruction[pair][1]
+        print(column, row)
+
 print(f'elapsed: {datetime.datetime.now() - start}')
