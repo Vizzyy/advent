d0279c76c656ab626951fc1806dd852d89cc9d96
Barney <barney@barney16.lan>
2024-12-05 18:34:16 -0500

    2024_05_part_1

--- boilerplate/date/puzzle.py
+++ boilerplate/date/puzzle.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+
+startTime = datetime.now()
+
 with open('test.txt', 'r') as file:
     inputs = file.read()
 
@@ -6,3 +10,5 @@ inputs = inputs.strip().split('\n')
 print(inputs)
 
 [print(input) for input in inputs]
+
+print(f'\nCompletion time: {datetime.now() - startTime}')
